4d1700edf0cba0e8cb8f56511f89ce821f55c9de
pp <34028312@qq.com>
2023-11-20 19:39:15 +0800

    2023-11-20 19:38:57 增加邮件服务器默认端口号

--- myproject/mysite/settings.py
+++ myproject/mysite/settings.py
@@ -235,7 +235,7 @@ CKEDITOR_UPLOAD_PATH = "ckeditor/uploads/"
 # 邮件服务器
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')  
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))  
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'),465)  
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'  
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
