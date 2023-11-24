ac4c16eeaaa7a291755dded9788bd09e808aea0d
pp <34028312@qq.com>
2023-11-24 12:39:38 +0800

    base model update

--- myproject/abstractapp/admin.py
+++ myproject/abstractapp/admin.py
@@ -20,24 +20,24 @@ class BaseModelAdmin(ModelAdmin):
         ]
     # model_name = self.model.__name__  # 这里获取模型的类名，即 "HomePage" 这里，self.model指向HomePage，因此self.model.__name__会返回字符串"HomePage"。
 
-@property
-def list_display_links(self):
-    """
-    Returns a list of display links for the model admin.
+    @property
+    def list_display_links(self):
+        """
+        Returns a list of display links for the model admin.
 
-    These links are obtained from the `list_display` attribute,
-    excluding any fields that are also present in `list_editable`.
+        These links are obtained from the `list_display` attribute,
+        excluding any fields that are also present in `list_editable`.
 
-    Returns:
-        list: A list of display links for the model admin.
-    """
+        Returns:
+            list: A list of display links for the model admin.
+        """
 
-    # Create a set of fields that can be edited in the list view
-    list_editable_set = set(self.list_editable)
+        # Create a set of fields that can be edited in the list view
+        list_editable_set = set(self.list_editable)
 
-    # Iterate over the fields in the `list_display` attribute
-    # and filter out the fields that are also present in `list_editable`
-    display_links = [field for field in self.list_display if field not in list_editable_set]
+        # Iterate over the fields in the `list_display` attribute
+        # and filter out the fields that are also present in `list_editable`
+        display_links = [field for field in self.list_display if field not in list_editable_set]
 
-    # Return the filtered list of display links
-    return display_links
+        # Return the filtered list of display links
+        return display_links
