10b86fc6fe59e229413721249a77a640e2c877a6
pp <34028312@qq.com>
2023-12-12 22:43:04 +0800

    debug

--- myproject/mysite/settings.py
+++ myproject/mysite/settings.py
@@ -144,28 +144,28 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db/db.sqlite3",
-        },
-        # "another_db": {
-        #     "ENGINE": "django.db.backends.sqlite3",
-        #     "NAME": BASE_DIR / "db copy.sqlite3",
-        # }
+# if DEBUG:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db/db.sqlite3",
+#         },
+#         # "another_db": {
+#         #     "ENGINE": "django.db.backends.sqlite3",
+#         #     "NAME": BASE_DIR / "db copy.sqlite3",
+#         # }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),  # 使用环境变量或默认值
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),  # 使用环境变量或默认值
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-            'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),  # 使用环境变量或默认值
-            'PORT': os.environ.get('POSTGRES_PORT', '5432'),  # 使用环境变量或默认值
-        }
-    }
+}
 
 
 
